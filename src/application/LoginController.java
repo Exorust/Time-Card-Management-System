e782c6ad4842af1696ac44c055f81612f8a52c40
ChetanVibhandik <ceasefire16@gmail.com>
2018-04-15 15:01:48 +0530

    button function

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,8 +1,16 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class LoginController {
 
@@ -16,4 +24,18 @@ public class LoginController {
     void initialize() {
 
     }
+    public void changeScreen(ActionEvent event) throws IOException
+    {
+    	Parent tableView = FXMLLoader.load(getClass().getResource("Emp.fxml"));
+    	Scene tableViewscene = new Scene(tableView);
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	window.setScene(tableViewscene);
+    	window.show();
+    }
+    /*void onLoginPress(ActionEvent event) 
+    {
+    		
+    	System.out.println("Hello");
+    }*/
 }
+
