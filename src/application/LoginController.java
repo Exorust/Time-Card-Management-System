a89b65464508edfc873850349a508137e07153a2
ChetanVibhandik <ceasefire16@gmail.com>
2018-04-15 15:33:25 +0530

    Switching works now

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -7,12 +7,16 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
-public class LoginController {
+public class LoginController implements Initializable{
 
     @FXML
     private ResourceBundle resources;
@@ -24,18 +28,33 @@ public class LoginController {
     void initialize() {
 
     }
-    public void changeScreen(ActionEvent event) throws IOException
+    @FXML 
+    private Button Login;
+    @FXML 
+    private Button ForgotPass;
+    @FXML 
+    private TextField userid;
+    @FXML 
+    private PasswordField passowrd;
+    public void changeScreen(ActionEvent event) throws IOException  
     {
-    	Parent tableView = FXMLLoader.load(getClass().getResource("Emp.fxml"));
+    	
+    	Parent tableView = FXMLLoader.load(getClass().getClassLoader().getResource("Emp.fxml"));
     	Scene tableViewscene = new Scene(tableView);
     	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
     	window.setScene(tableViewscene);
     	window.show();
+    	
     }
     /*void onLoginPress(ActionEvent event) 
     {
     		
     	System.out.println("Hello");
     }*/
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
