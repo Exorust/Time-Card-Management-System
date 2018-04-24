3fa6109197358c5f326b42c67066a06826ca6236
gopeshkh1 <f20160101@hyderabad.bits-pilani.ac.in>
2018-04-24 22:48:03 +0530

    IN/OUT

--- src/application/EmpController.java
+++ src/application/EmpController.java
@@ -7,6 +7,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -122,10 +124,110 @@ public class EmpController {
     public void in (ActionEvent event) throws IOException
     {
     	// Enter the code here for Time IN
+    	Calendar cal = Calendar.getInstance();
+    	cal.add(Calendar.DATE, 1);
+    	SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+    	String date = format1.format(cal.getTime());
+    	SimpleDateFormat format2 = new SimpleDateFormat("HH:mm:ss");
+    	String time = format2.format(cal.getTime());
+    	Connection conn = null;
+  	   Statement stmt = null;
+  	   try{
+  	      //STEP 2: Register JDBC driver
+  	      Class.forName("com.mysql.jdbc.Driver");
+
+  	      //STEP 3: Open a connection
+  	      System.out.println("Connecting to database for emp...");
+  	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+  	      //STEP 4: Execute a query
+  	      System.out.println("Creating statement for name...");
+  	      stmt = conn.createStatement();
+  	      String sql;
+  	      sql = "insert into works values (eid,'"+date+"','"+time+"',null)";
+  	      stmt.executeUpdate(sql);
+  	      
+  	      
+
+  	      
+  	      //STEP 6: Clean-up environment
+  	      rs.close();
+  	      stmt.close();
+  	      conn.close();
+  	   }catch(SQLException se){
+  	      //Handle errors for JDBC
+  	      se.printStackTrace();
+  	   }catch(Exception e){
+  	      //Handle errors for Class.forName
+  	      e.printStackTrace();
+  	   }finally{
+  	      //finally block used to close resources
+  	      try{
+  	         if(stmt!=null)
+  	            stmt.close();
+  	      }catch(SQLException se2){
+  	      }// nothing we can do
+  	      try{
+  	         if(conn!=null)
+  	            conn.close();
+  	      }catch(SQLException se){
+  	         se.printStackTrace();
+  	      }//end finally try
+  	   }//end try 
+    	
     }
     public void out (ActionEvent event) throws IOException
     {
     	// Enter the code for Time OUT
+    	Calendar cal = Calendar.getInstance();
+    	cal.add(Calendar.DATE, 1);
+    	SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+    	String date = format1.format(cal.getTime());
+    	SimpleDateFormat format2 = new SimpleDateFormat("HH:mm:ss");
+    	String time = format2.format(cal.getTime());
+    	
+   	   try{
+   	      //STEP 2: Register JDBC driver
+   	      Class.forName("com.mysql.jdbc.Driver");
+
+   	      //STEP 3: Open a connection
+   	      System.out.println("Connecting to database for emp...");
+   	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+   	      //STEP 4: Execute a query
+   	      System.out.println("Creating statement for name...");
+   	      stmt = conn.createStatement();
+   	      String sql;
+   	      sql = "update works set checkout="+date+"where eid="+ID;
+   	      stmt.executeUpdate(sql);
+   	      
+   	      
+
+   	      
+   	      //STEP 6: Clean-up environment
+   	      rs.close();
+   	      stmt.close();
+   	      conn.close();
+   	   }catch(SQLException se){
+   	      //Handle errors for JDBC
+   	      se.printStackTrace();
+   	   }catch(Exception e){
+   	      //Handle errors for Class.forName
+   	      e.printStackTrace();
+   	   }finally{
+   	      //finally block used to close resources
+   	      try{
+   	         if(stmt!=null)
+   	            stmt.close();
+   	      }catch(SQLException se2){
+   	      }// nothing we can do
+   	      try{
+   	         if(conn!=null)
+   	            conn.close();
+   	      }catch(SQLException se){
+   	         se.printStackTrace();
+   	      }//end finally try
+   	   }//end try 
     }
     public void status (ActionEvent event) throws IOException
     {
